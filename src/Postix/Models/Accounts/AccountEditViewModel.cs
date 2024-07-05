239a70751ebdd22b651e1ebc0ba1c79f6bb443ec
to-wer <tw@to-wer.dev>
2024-07-05 07:40:07 +0200

    Moved AccountEditViewModel to correct namespace

--- src/Postix/Models/Accounts/AccountEditViewModel.cs
+++ src/Postix/Models/Accounts/AccountEditViewModel.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Postix.Models;
+namespace Postix.Models.Accounts;
 
 public class AccountEditViewModel
 {
